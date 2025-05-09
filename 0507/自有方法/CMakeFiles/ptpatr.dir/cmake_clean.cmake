049bdd65c172e30c6c03a52e991c8f1484549e0a
unknown <1830602377@qq.com>
2025-05-09 15:40:59 +0800

    提交基线测试分析

---
+++ 0507/自有方法/CMakeFiles/ptpatr.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ptpatr.dir/src/main.cpp.o"
+  "CMakeFiles/ptpatr.dir/src/main.cpp.o.d"
+  "ptpatr"
+  "ptpatr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ptpatr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
